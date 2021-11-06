e2081f07f454620cd15dbe178315d4f875c020f0
Calvin <zxw880507@gmail.com>
2021-11-05 18:41:06 -0600

    add curd thunks

--- store/features/productFetchingSlice.js
+++ store/features/productFetchingSlice.js
@@ -16,6 +16,36 @@ export const getProductFetchingList = createAsyncThunk(
   }
 );
 
+export const addProductFetch = createAsyncThunk(
+  "addProductFetch",
+  async (fetchForm, { rejectWithValue }) => {
+    try {
+      const res = await axios.post(`/api/userId`, { fetchForm });
+      return res.data;
+    } catch (error) {
+      if (!error.response) {
+        throw err;
+      }
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const deleteProductFetch = createAsyncThunk(
+  "deleteProductFetch",
+  async (id, { rejectWithValue }) => {
+    try {
+      const res = await axios.delete(`/api/userId`, { data: { id } });
+      return res.data;
+    } catch (error) {
+      if (!error.response) {
+        throw err;
+      }
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
 const initialState = {
   productFetchingList: [],
   status: "idle",
@@ -36,6 +66,41 @@ const productFetchingSlice = createSlice({
         state.productFetchingList = action.payload;
       })
       .addCase(getProductFetchingList.rejected, (state, action) => {
+        state.status = "failed";
+        if (action.payload) {
+          state.error = action.payload.errorMessage;
+        } else {
+          state.error = action.error.message;
+        }
+      })
+      .addCase(addProductFetch.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(addProductFetch.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.productFetchingList.push(action.payload);
+      })
+      .addCase(addProductFetch.rejected, (state, action) => {
+        state.status = "failed";
+        if (action.payload) {
+          state.error = action.payload.errorMessage;
+        } else {
+          state.error = action.error.message;
+        }
+      })
+      .addCase(deleteProductFetch.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(deleteProductFetch.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        const { id } = action.payload;
+        const index = state.productFetchingList.findIndex((el) => el.id === id);
+        if (index === -1) {
+          return state;
+        }
+        state.productFetchingList.splice(index, 1);
+      })
+      .addCase(deleteProductFetch.rejected, (state, action) => {
         state.status = "failed";
         if (action.payload) {
           state.error = action.payload.errorMessage;
