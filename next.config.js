a39773234a9cf2c2890065c164c5f3e5be0bba3a
Calvin <zxw880507@gmail.com>
2021-11-13 14:17:01 -0700

    env client config

--- next.config.js
+++ next.config.js
@@ -5,4 +5,8 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  env: {
+    PUSHER_KEY: process.env.PUSHER_KEY,
+    PUSHER_CLUSTER: process.env.PUSHER_CLUSTER,
+  },
 };
